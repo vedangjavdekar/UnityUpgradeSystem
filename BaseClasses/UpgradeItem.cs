970c296f91a8bba36db3e9681ae21e5474e6f0ea
vedangasDARKNINJA <vedang.javdekar@gmail.com>
2021-03-30 20:07:14 +0530

    Fixes in base classes. More robust implementation

--- BaseClasses/UpgradeItem.cs
+++ BaseClasses/UpgradeItem.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class UpgradeItem : ScriptableObject
+public abstract class UpgradeItem : ScriptableObject, IUpgradeItemMethods
 {
     public string upgradeName;
     public int currentLevel;
+
+    public virtual bool canUpgrade { get; }
+    public virtual bool reachedMaxLevel { get; }
+
+    public virtual void Upgrade()
+    {
+        if (canUpgrade)
+        {
+            currentLevel++;
+        }
+    }
 }
